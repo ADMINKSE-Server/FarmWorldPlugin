d70f755fbfa60e7a371d4af8ca8fa94335382557
Kruemmelspalter <mika.thomas@freenet.de>
2020-09-13 16:33:43 +0200

    Removed debug statements

--- src/main/kotlin/net/adminkse/plugins/farmworld/listeners/PlayerChangedWorldListener.kt
+++ src/main/kotlin/net/adminkse/plugins/farmworld/listeners/PlayerChangedWorldListener.kt
@@ -14,13 +14,12 @@ class PlayerChangedWorldListener(var plugin: JavaPlugin) : Listener {
     @EventHandler
     public fun onPlayerChangedWorld(event: PlayerChangedWorldEvent) {
         if(event.player.world.name==plugin.config.get("worldName")) {
-            println("Event triggered")
             var r = 100 * sqrt(Math.random())
             var a = Math.random() * 2 * Math.PI
             var x = r * cos(a) + event.player.location.x
             var z = r * sin(a) + event.player.location.z
             var y = event.player.world.getHighestBlockYAt(x.toInt(), z.toInt()).toDouble()
             event.player.teleport(Location(event.player.world, x, y, z))
-        } else println(event.player.world.name + " : "+plugin.config.get("worldName"))
+        }
     }
 }
